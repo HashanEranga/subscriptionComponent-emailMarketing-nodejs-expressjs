dfee8230ba998692fc3e9f8e67b9db267a34df16
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2022-01-24 22:39:36 +0530

    feat(): submit button workings

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+
 const app = express()
 const PORT = 3000
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/signup.html")
+})
+
+app.post("/", function(req,res){
+    const firstName = req.body.FName
+    const lastName = req.body.LName
+    const email = req.body.inputEmail
+    res.send(`
+        <h1>First Name : ${firstName}</h1>
+        <h1>Last Name : ${lastName}</h1>
+        <h1>Email : ${email}</h1>
+    `)
 })
 
 
